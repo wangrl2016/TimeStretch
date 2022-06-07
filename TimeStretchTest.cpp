38ae07b27e23cb0986f4f8a00d26869005c17285
wangrl2016 <wangrl2016@gmail.com>
2022-06-07 23:21:25 +0800

    进行变速调用

--- TimeStretchTest.cpp
+++ TimeStretchTest.cpp
@@ -5,8 +5,12 @@
 #include <cassert>
 #include <fstream>
 #include <vector>
+#include <tuple>
 #include "glog/logging.h"
 #include "sox.h"
+#include "server_base/audio_utils.h"
+
+using namespace WL::Service::Base;
 
 int main(int argc, char* argv[]) {
     // Initialize Google’s logging library.
@@ -18,7 +22,7 @@ int main(int argc, char* argv[]) {
     std::ifstream in;
     in.open("/home/wangrl/github/TimeStretch/data", std::ios::binary);
 
-    std::vector<char> data;
+    std::vector<char> dataVec;
 
     if (in.is_open()) {
         LOG(INFO) << "Read data success";
@@ -26,20 +30,41 @@ int main(int argc, char* argv[]) {
 
     char c;
     while (in.read(&c, sizeof(c))) {
-        data.push_back(c);
+        dataVec.push_back(c);
     }
 
-    LOG(INFO) << "Data size " << data.size();
+    LOG(INFO) << "Data size " << dataVec.size();
 
     // byte为单位
-    assert(data.size() == 104960);
+    assert(dataVec.size() == 104960);
 
     int dataSize = 104960;
 
-    std::ofstream outStream("data.out",
+    char* dataArr = (char*) calloc(104960, sizeof(char));
+
+    for (int i = 0; i < dataVec.size(); i++) {
+        dataArr[i] = dataVec[i];
+    }
+
+    // 对音频流进行处理
+    std::vector<std::tuple<std::string, int, int>> soxList;
+    std::tuple<std::string, int, int> temp = {"tempo=0.94", dataSize, 65};
+    soxList.push_back(temp);
+
+    snd_file sndFile = process_sox_chain_list(soxList,
+                                              dataArr,
+                                              dataSize,
+                                              "mp3");
+
+    LOG(INFO) << "sndFile offset " << sndFile.offset
+            << ", size " << sndFile.size
+            << ", timems " << sndFile.timems;
+
+
+    std::ofstream outStream("dataVec.out",
                             std::ios::out | std::ios::binary);
 
-    for (const char& c : data) {
+    for (const char& c : dataVec) {
         outStream.write(&c, sizeof c);
     }
 
